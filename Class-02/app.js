7368f4f5942b58acf117897f79157248ed47fbcf
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-06-22 21:52:16 -0700

    same js added

---
+++ Class-02/app.js
@@ -0,0 +1,15 @@
+// let userName = "Farhan Nawaz"
+// console.log(userName)
+
+// let num1 = 3
+// let num2 = 5
+// console.log(num1 + num2)
+
+
+// let a = 5
+// let b = "9"
+// console.log((a + b) * b - a + b + a)
+
+
+let favDish = prompt("Enter Your fav dish")
+console.log(favDish)

ecfd21e16db800a0a8893dd98e9c6f8648a7c933
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-06-22 22:47:08 -0700

    some js added

--- Class-02/app.js
+++ Class-02/app.js
@@ -22,7 +22,7 @@
 
 
 
-let a = 2
+// let a = 2
 // console.log(a++)
 // console.log(a)
 // console.log(++a)
@@ -32,4 +32,33 @@ let a = 2
 // console.log(a)
 // console.log(--a)
 
-console.log(--a + a++ + a++ + --a + ++a + a++)
+// console.log(--a + a++ + a++ + --a + ++a + a++)
+
+// Today Sir Assignment
+let user = +prompt("enter your Table Number" , "2")
+// console.log(user + " x " + " 1 " + " = " + user*1);
+// console.log(user + " x " + " 2 " + " = " + user*2);
+// console.log(user + " x " + " 3 " + " = " + user*3);
+// console.log(user + " x " + " 4 " + " = " + user*4);
+// console.log(user + " x " + " 5 " + " = " + user*5);
+// console.log(user + " x " + " 6 " + " = " + user*6);
+// console.log(user + " x " + " 7 " + " = " + user*7);
+// console.log(user + " x " + " 8 " + " = " + user*8);
+// console.log(user + " x " + " 9 " + " = " + user*9);
+// console.log(user + " x " + " 10 " + " = " + user*10);
+
+let firstName = 0;
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+console.log(user + " x " + ++firstName + " = " + user*firstName);
+

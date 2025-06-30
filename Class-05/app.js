0550a1c5242c1c46d5a51aa7b1c5defdad17b408
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-06-29 22:10:50 -0700

    class-05

---
+++ Class-05/app.js
@@ -0,0 +1,43 @@
+// Object
+// let secName= "Nawaz"
+
+// let obj = {
+//     name: "farhan",
+//     age: 21,
+//     rollNo: 32489126,
+//     call: "031211331",
+
+// }
+// console.log(obj)
+
+
+
+
+
+// let num;
+
+// let obj5 = {
+// name: "muddasir",
+// fName: "hayat",
+// class: "WebDev",
+// }
+
+// let Value = obj5.num = 987654321
+
+// console.log(obj5);
+// console.log(Value);
+
+
+
+
+// let newValue= "key 1"
+// let obj = {
+//     name: "ali",
+//     fname: "ahmed",
+//     calss: "WebDev",
+// }
+// let newobj = (obj[newValue] = "Rahman", obj)
+// console.log(newobj)
+
+
+

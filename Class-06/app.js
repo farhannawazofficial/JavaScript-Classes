4d753626713050e0391226f258d2e44b973d4746
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-01 21:28:32 -0700

    class-06

---
+++ Class-06/app.js
@@ -0,0 +1,10 @@
+let addedStudent = "fivthStudent"
+
+let student = {
+    firstStudent: "ali",
+    secoundStudent: "Ahmad",
+    thirdStudent: "Raza",
+    forthStudent: "syed",
+}
+let newStudent = (student[addedStudent], "Farhan", student)
+console.log(student[addedStudent], "Farhan", student)

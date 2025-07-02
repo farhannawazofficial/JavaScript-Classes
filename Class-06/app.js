15c9f9a3f6f9c887c471c72fe629c97b7eb2a02c
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-01 22:00:24 -0700

    tasks added

--- Class-06/app.js
+++ Class-06/app.js
@@ -12,13 +12,31 @@ console.log(student[addedStudent], "Farhan", student)
 
 
 //funcations
-function cook (){
-    console.log("Your Baryani is Ready")
-}
-cook()
+// function cook (){
+//     console.log("Your Baryani is Ready")
+// }
+// cook()
 
-function sum (num1, num2){
-    console.log(num1 + num2)
+// function sum (num1, num2){
+//     console.log(num1 + num2)
+// }
+// sum(2,5)
+// sum(7,5)
+
+// enter your user data
+// function sum (num1, num2){
+//     console.log(num1 + num2)
+// };
+
+// let userinput1 = +prompt("enter Your first Number")
+// let userinput2 = +prompt("enter Your secound Number")
+
+// sum(userinput1, userinput2)
+
+
+function data (firstNmae, secoundName, ){
+    console.log("Welcome " + firstNmae, secoundName)
 }
-sum(2,5)
-sum(7,5)
+let userinput1 = prompt("enter your first name")
+let userinput2 = prompt("enter your secound name")
+data(userinput1, userinput2)

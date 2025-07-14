321ee183cd33101eb0807f14855cd0ec9045499e
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-13 22:02:30 -0700

    class-09

---
+++ Class-09/app.js
@@ -0,0 +1,27 @@
+//loops
+
+// let h1 = document.getElementById("text")
+// for (let i = 0; i < 11; i++) {
+//     h1.innerHTML = i
+//     console.log("Hello World", i)
+
+// }
+// let loopStart = +prompt("enter Your start Number")
+// let loopEnd = +prompt("enter Your end Number")
+
+// for (let i = loopStart; i < loopEnd; i++) {
+//     console.log(i)
+
+// }
+
+
+// let loopStart = +prompt("enter Your start Number")
+// let loopEnd = +prompt("enter Your end Number")
+// let iLaps = +prompt("which skips numers")
+
+// for (let i = loopStart; i < loopEnd; i = i + iLaps) {
+//     console.log(i)
+
+// }
+
+

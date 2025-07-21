e0908d86518b924b7a8926c870422bcbcbefdb56
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-20 23:06:36 -0700

    Update app.js

--- Class-11/app.js
+++ Class-11/app.js
@@ -23,8 +23,22 @@
 
 
 
-let str = "*******";
-for(let i = 0; i<7; i++){
-    document.write("<h1>" + str + "</h1>")
-    str = str.slice(0 , str.length -1)
-}
+// let str = "*******";
+// for(let i = 0; i<7; i++){
+//     document.write("<h1>" + str + "</h1>")
+//     str = str.slice(0 , str.length -1)
+// }
+
+
+
+
+//  let number = +prompt("Enter Your Table");
+//     for (let i = 1; i <= 10; i++) {
+//       console.log(number + " × " + i + " = " + number * i );
+//     }
+
+
+let birthMonth = ["jan", "feb", "mar", "apr", "may" ];
+
+userBirthMonth = userBirthMonth.slice(0, 3)
+userBirthMonth = userBirthMonth.toLowerCase()

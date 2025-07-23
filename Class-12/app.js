d1261b7a02b5527b639ade2dc3d4ea7af65d3846
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-22 22:28:12 -0700

    new updated

--- Class-12/app.js
+++ Class-12/app.js
@@ -1,2 +1,22 @@
-let discountedMonths = ["jan", "feb", "mar", "apr", "may"]
-let userBirthMonth = prompt ("Enter Your Birth Month?")
+window.onload = function() {
+    let disMonth = ["jan", "feb", "mar", "apr", "may", "jun"];
+    let userBirthMonth = prompt("Enter your Birthday month");
+
+    userBirthMonth = userBirthMonth.toLowerCase().slice(0, 3);
+    let birthMonth = false;
+
+    let p = document.getElementById("text");
+    for (let i = 0; i < disMonth.length; i++) {
+        if (disMonth[i] === userBirthMonth) { // Fixed comparison logic
+            birthMonth = true;
+            break;
+        }
+    }
+
+    if (birthMonth) {
+        p.innerHTML = "You are eligible for the birthday discount!";
+    }
+    else {
+        p.innerHTML = "Sorry apny paida hony mei dair krdi";
+    }
+};

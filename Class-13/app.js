6b674d78c691c8cd89a6494db2fd17b1a4d0ca7f
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-27 22:09:10 -0700

    class-13

---
+++ Class-13/app.js
@@ -0,0 +1,27 @@
+
+
+// // genreate the 1 number
+// let randomNumber = Math.floor((Math.random() * 6) + 1)
+// console.log (randomNumber)
+
+// // Math.random()
+// // Math.round()
+// // Math.floor()
+// // Math.ceil()
+ 
+
+// function convertToFahrenheit() {
+//   let celsiusInput = document.getElementById("celsius").value;
+//   let celsius = parseFloat(celsiusInput);
+
+//   if (isNaN(celsius)) {
+//     document.getElementById("result").innerText = "Please enter a valid number.";
+//     return;
+//   }
+
+//   let fahrenheit = (celsius * 9/5) + 32;
+//   document.getElementById("result").innerText = 
+//     `${celsius}°C is equal to ${fahrenheit.toFixed(2)}°F`;
+// }
+
+

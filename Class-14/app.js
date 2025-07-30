b5712797b2289bde41c0dfbb39fb194164260537
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-07-29 22:48:49 -0700

    class-14

---
+++ Class-14/app.js
@@ -0,0 +1,28 @@
+// let discount = 70; 
+
+// let product1 = "23000TV";
+// let product2 = "267530Mobile";
+// let product3 = "267830Laptop";
+
+// let price1 = parseFloat(product1);
+// let price2 = parseFloat(product2);
+// let price3 = parseFloat(product3);
+
+// let discounted1 = (price1 - (price1 * discount / 100));
+// let discounted2 = (price2 - (price2 * discount / 100));
+// let discounted3 = (price3 - (price3 * discount / 100));
+
+// console.log( discounted1);
+// console.log( discounted2);
+// console.log( discounted3);
+
+
+// let age = prompt("Enter Your age")
+// age = parseInt(age)
+// if (age >= 18) {
+//     console.log("Your are avalivale")
+// } else if (age < 18) {
+//     console.log("Your are not avaloabe")
+// } else {
+//     console.log("Invalid character")
+// }

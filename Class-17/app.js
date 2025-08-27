c3486100584beabfd62eb04c7964c0fad073b9d9
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-08-27 09:50:10 +0500

    Update app.js

--- Class-17/app.js
+++ Class-17/app.js
@@ -2,4 +2,22 @@
 // nodeType                          element = 1 , Text = 3
 // nodeValue                        Importants
 // nodeName                         <em></em> = EM
-// 
+
+
+// create the html in js
+
+// //create the html element and add the text in the element 
+// let h1 = document.createElement("h1")
+// let para = document.createTextNode("This is Para")
+// h1.appendChild(para)
+// console.log(h1)
+// //adding the text in DOM
+// let bodyEL = document.childNodes[1].childNodes[2]
+// bodyEL.appendChild(h1)
+
+
+
+
+
+
+

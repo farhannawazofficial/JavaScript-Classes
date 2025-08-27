eadf674d5c4a1c4706d87a26a749cfdc237971c9
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2025-08-27 09:35:25 +0500

    class updated

---
+++ Class-16/app.js
@@ -0,0 +1,4 @@
+let nodeEL = document.childNodes[1].childNodes[2].childNodes[1];
+let paraEL = nodeEL.innerHTML = "Tis is para"
+
+console.log(paraEL)
---
+++ Class-17/app.js
@@ -0,0 +1,5 @@
+// childNode []
+// nodeType                          element = 1 , Text = 3
+// nodeValue                        Importants
+// nodeName                         <em></em> = EM
+// 
